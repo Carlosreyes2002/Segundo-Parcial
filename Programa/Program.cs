ecd9bcd77fd4ffea8883a319a044fd24c0bef00d
Carlos Reyes <carlosrrg2002@gmail.com>
2023-05-14 19:09:25 -0600

    segundo parcial

---
+++ Programa/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program {
+  static string[] palabras;
+
+  static void Main(string[] args) {
+    palabras = new string[5];
+    Insertar(palabras, "Hola", 0);
+    Insertar(palabras, "Mundo", 1);
+    Mostrar(palabras);
+    Console.ReadLine();
+  }
+
+  static void Mostrar(string[] arreglo) {
+    foreach (string palabra in arreglo) {
+      Console.WriteLine(palabra);
+    }
+  }
+
+  static void Insertar(string[] arreglo, string elemento, int indice) {
+    if (indice >= 0 && indice < arreglo.Length) {
+      arreglo[indice] = elemento;
+    } else {
+      Console.WriteLine("El indice es invalido");
+    }
+  }
+}
